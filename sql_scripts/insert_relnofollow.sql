2c5166e4c570c7dadbaedc5fa8339e1a5e16918c
Albanis Pacheco <apena@northsouthstudios.com>
2017-07-05 16:49:51 +0000

    insert_relnofollow.sql deleted online with Bitbucket

--- sql_scripts/insert_relnofollow.sql
+++
@@ -1,8 +0,0 @@
-TRUNCATE TABLE wp_item_rel_nofollow;
-INSERT INTO wp_item_rel_nofollow (item_id,is_rel_nofollow)
-SELECT ID, 1
-from wp_posts
-WHERE post_type = 'item'
-UNION
-SELECT itemId, 1
-from wp_apicache_items
