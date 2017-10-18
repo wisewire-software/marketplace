18e3046e8b9398e6549159e16236b9e5197a70eb
anabelespinal <ayec22_09@hotmail.com>
2017-10-18 15:34:12 -0500

    Create template to Thank You page, and adding images for that.

---
+++ wp-content/themes/wisewire/template-thank-you.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template Name: Thank you
+ */
+
+?>
+
+<?php get_header(); ?>
+
+<section class="section-thank-you">
+    <div class="container text-center">
+        <div class="row">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/icon_check.png">
+            <h1 class="title-thank-you">Thank You For Contacting Wisewire</h1>
+            <p class="lead description-ty first-description-ty">
+                One of our representative swill call you shortly.
+            </p>
+            <p class="lead description-ty">
+                In the meantime, feel free to browse our marketplace, read our blog,
+                or interact with our social community!
+            </p>
+        </div>
+    </div>
+    <div class="container content-links-ty text-center">
+        <div class="col-links">
+            <a href="#">
+                <img class="hidden-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_wisewire.png">
+                <img class="visible-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_wisewire_2.png">
+            </a>
+            <a href="#">
+                <img class="hidden-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_blog.png">
+                <img class="visible-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_blog_2.png">
+            </a>
+            <a href="#">
+                <img class="hidden-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_linkedin.png">
+                <img class="visible-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_linkedin_2.png">
+            </a>
+            <a href="#">
+                <img class="hidden-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_facebook.png">
+                <img class="visible-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_facebook_2.png">
+            </a>
+            <a href="#">
+                <img class="hidden-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_twitter.png">
+                <img class="visible-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_twitter_2.png">
+            </a>
+            <a href="#">
+                <img class="hidden-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_pinterest.png">
+                <img class="visible-xs" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/btn_pinterest_2.png">
+            </a>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
