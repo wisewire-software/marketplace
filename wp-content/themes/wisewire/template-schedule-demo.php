df59d5925ae5c506a83f911f13108e545e364960
anabelespinal <ayec22_09@hotmail.com>
2017-10-18 13:18:10 -0500

    Create template to Schedule Demo page, and change style for that.

--- wp-content/themes/wisewire/template-schedule-demo.php
+++ wp-content/themes/wisewire/template-schedule-demo.php
@@ -18,8 +18,8 @@
     </div>
 </section>
 <section class="general-section-schedule">
-    <div class="container">
-        <div class="col-sm-7">
+    <div class="container col-form-sd">
+        <div class="col-sm-7 hidden-xs">
             <h2 class="title-general-section">Schedule Your Demo with Wisewire</h2>
             <div class="content-description-schedule">
                 Talk with one of our sales reps and have them walk you through Wisewire's
@@ -46,8 +46,45 @@
                 <img class="icon-schedule img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/ICON4_APU_2.png">
             </div>
         </div>
+        <div class="col-sm-5 col-form-sd">
+            <div class="content-form-sd">
+                <div class="description-form-sd">
+                    Take a deeper dive into Wisewire's learning
+                    platform and solutions? Fill out this form to
+                    talk to one of our experts.
+                </div>
+                <div class="content-fields-form-sd">
+                    <?php echo do_shortcode('[contact-form-7 id="69620" title="Schedule Demo"]') ?>
+                </div>
+                <div class="footer-form-sd">
+                    100% Privacy Guaranteed
+                </div>
+            </div>
+        </div>
     </div>
-    <?php echo do_shortcode('[contact-form-7 id="69620" title="Schedule Demo"]') ?>
+    <div class="container visible-xs col-show-sd">
+        <div class="col-xs-12">
+            <h2 class="title-general-section">Schedule Your Demo with Wisewire</h2>
+            <div class="content-description-schedule">
+                Talk with one of our sales reps and have them walk you through Wisewire's
+                solution and partnership offerings. Your sales rep can demo our platform
+                (which houses tens of thousands of learning resources and technology -
+                enhanced items), talk through LMS integration, customization of content,
+                creation of new content, and more!
+            </div>
+            <div class="content-description-schedule">
+                Simply fill out the form and a Wisewire representative will be in touch with
+                you shortly to schedule a demonstration.
+            </div>
+            <h3 class="trusted-schedule">TRUSTED BY THE TOP NAMES IN EDUCATION</h3>
+            <div class="content-icons-schedule">
+                <img class="icon-schedule img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/ICON1_kaplan_2.png">
+                <img class="icon-schedule img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/ICON2_openstax_2.png">
+                <img class="icon-schedule img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/ICON3_pearson_2.png">
+                <img class="icon-schedule img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/b2b_pages/ICON4_APU_2.png">
+            </div>
+        </div>
+    </div>
 </section>
 
 <?php get_footer(); ?>
