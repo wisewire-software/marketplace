2b0ab1a2e4d8f8fb9fa37f28e1b93c502df9300b
Jose Luis Collantes <jcollantes@northsouthstudios.com>
2017-11-01 21:15:19 -0500

    Fix re-captcha test 4

--- wp-content/themes/wisewire/template-user-login.php
+++ wp-content/themes/wisewire/template-user-login.php
@@ -91,7 +91,6 @@
       if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
         $secret = '6LfIxjYUAAAAANyJIjRdhnyydgDuQxYKOwCDajI0';
         $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
-        echo $verifyResponse;
         $responseData = json_decode($verifyResponse);
         if($responseData->success) {
           return true;
